00f2485dc14b0d6b761cac222f1a4fbe93e79026
sockarockee <taylorg8900@gmail.com>
2025-04-07 10:16:28 -0600

    dicts.py - updated functions

--- dicts.py
+++ dicts.py
@@ -1,6 +1,6 @@
 
 
-def create_values_dict(filename):
+def create_values_dict(filename, key_index, value_index):
     '''
     Takes a file and creates a dictionary which contains float values for each of the 34 effects in the game
     '''
@@ -9,13 +9,13 @@ def create_values_dict(filename):
     dict = {}
     for line in f:
         fields = line.split(',')
-        key = fields[0].strip()
-        value = round(float(fields[1].strip().strip('\n')), 2)
+        key = fields[key_index].strip()
+        value = round(float(fields[value_index].strip().strip('\n')), 2)
         dict[key] = value
     f.close()
     return dict
 
-def create_mixing_dict(filename):
+def create_modify_dict(filename, dict_index, key_index, value_index):
     '''
     Takes a file and creates a dictionary of dictionaries.
     Each value in the subdictionary is what the key turns into, when the dictionary name modifies the key.
@@ -38,16 +38,16 @@ def create_mixing_dict(filename):
     dict = {}
     for line in f:
         fields = line.split(',')
-        og_effect = fields[0].strip()
-        new_effect = fields[1].strip()
-        mixer = fields[2].strip().strip('\n')
+        og_effect = fields[key_index].strip()
+        new_effect = fields[value_index].strip()
+        mixer = fields[dict_index].strip().strip('\n')
         if mixer not in dict:
             dict[mixer] = {}
         dict[mixer][og_effect] = new_effect
     f.close()
     return dict
 
-def create_mixer_dict(filename):
+def create_mixer_dict(filename, key_index, value_index):
     '''
     Takes a file and creates a dictionary, where the key is a mixer and the value is the effect it passively adds to a drug.
     For example, 'chili' will add 'spicy'.
@@ -62,8 +62,8 @@ def create_mixer_dict(filename):
     dict = {}
     for line in f:
         fields = line.split(',')
-        mixer = fields[0].strip()
-        effect = fields[1].strip().strip('\n')
+        mixer = fields[key_index].strip()
+        effect = fields[value_index].strip().strip('\n')
         dict[mixer] = effect
     f.close()
     return dict
