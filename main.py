b391b82350b5dc9c61d18c1a1d8e938b61c7f1f0
sockarockee <taylorg8900@gmail.com>
2025-04-07 17:50:24 -0600

    main.py - code duplication, but have a find worst drug function now

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from drug import Drug
 from dicts import create_mixer_dict
+import sys
 
 def main():
     drug = Drug('weed', ['athletic', 'calming'])
     # drug.add_mixer('mega bean')
-    strains = find_best_strains(drug, 16)
+    best_strains = find_best_strains(drug, 3)
+    worst_strains = find_worst_strains(drug, 2)
+    strains = [] + best_strains + worst_strains
     print(drug.get_mult())
     print(drug.get_effects())
 
@@ -20,6 +23,7 @@ def find_best_strains(drug_object, amount):
     mixer_dict = create_mixer_dict('csv-files/mixer.csv', 0, 1)
     strains = []
     best_strains = []
+    worst_strain = 0
 
     # Create a new strain from every possible mixer in the game
     for mixer in mixer_dict:
@@ -39,6 +43,29 @@ def find_best_strains(drug_object, amount):
         best_strains.append(best)
     return best_strains
 
+
+def find_worst_strains(drug_object, amount):
+    mixer_dict = create_mixer_dict('csv-files/mixer.csv', 0, 1)
+    strains = []
+    worst_strains = []
+
+    # Create a new strain from every possible mixer in the game
+    for mixer in mixer_dict:
+        new_drug = Drug(drug_object.get_type(), drug_object.get_effects())
+        new_drug.add_mixer(mixer)
+        strains.append(new_drug)
+
+    # Compare all new strains to find which ones have the best value
+    for i in range(amount):
+        min = 100_000
+        worst = strains[0]
+        for strain in strains:
+            if strain.get_mult() < min:
+                min = strain.get_mult()
+                worst = strain
+        strains.remove(worst)
+        worst_strains.append(worst)
+    return worst_strains
 
 main()
 
