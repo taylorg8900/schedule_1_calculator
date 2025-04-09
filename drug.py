766c11dcdea688d4c103cce7becfba7d6b9a0287
sockarockee <taylorg8900@gmail.com>
2025-04-09 16:43:15 -0600

    drugs can now have 0 effects to start with

--- drug.py
+++ drug.py
@@ -3,7 +3,7 @@ from dicts import create_modify_dict, create_mixer_dict, create_values_dict
 MAX_EFFECTS = 8
 
 class Drug:
-    def __init__(self, drug_type, effects):
+    def __init__(self, drug_type, effects=[]):
         self._type = drug_type
         self._mult = 0
         self._value = 0
@@ -19,11 +19,87 @@ class Drug:
 
         self.__create_dicts()
 
-        for effect in effects:
-            self._effects[effect] = self._value_dict[effect]
+        if len(effects) > 0:
+            for effect in effects:
+                self._effects[effect] = self._value_dict[effect]
 
         self.__calc_mult()
         self.__calc_value()
+
+    def add_mixer(self, mixer):
+        self._added_mixer = mixer.lower()
+        self._added_effect = self._mixer_dict[self._added_mixer]
+        self.__create_modified_effects_dict(mixer)
+        self.__modify_effects(mixer)
+
+        mult_before = self._mult
+        self.__calc_mult()
+        self._added_mult = round(self._mult - mult_before, 2)
+        self.__calc_value()
+
+
+    def __create_modified_effects_dict(self, mixer):
+        dict = {}
+        for effect in self._effects:
+            if effect in self._modify_dict[mixer]:
+                dict[effect] = self._modify_dict[mixer][effect]
+        self._modified_effects = dict
+
+    def __modify_effects(self, mixer):
+        new_effects = {}
+        for effect in self._effects:
+            if effect in self._modify_dict[mixer]:
+                new_effects[self._modify_dict[mixer][effect]] = self._value_dict[self._modify_dict[mixer][effect]]
+            else:
+                new_effects[effect] = self._effects[effect]
+
+        # We include this because if that effect already exists, we don't want to add it a second time.
+        # We also don't want to have more than the maximum allowed number of effects on a drug.
+        if len(self._effects) < MAX_EFFECTS:
+            new_effects[self._added_effect] = self._value_dict[self._mixer_dict[self._added_mixer]]
+        new_effects = dict(sorted(new_effects.items()))
+        self._effects = new_effects
+
+
+    def __create_dicts(self):
+        self._value_dict = create_values_dict('csv-files/values.csv', 0, 1)
+        self._mixer_dict = create_mixer_dict('csv-files/mixer.csv', 0, 1)
+        self._modify_dict = create_modify_dict('csv-files/modify.csv', 2, 0, 1)
+
+    def __calc_mult(self):
+        total = 1
+        for effect in self._effects:
+            total += self._effects[effect]
+        self._mult = round(total, 2)
+
+    def __calc_value(self):
+        drug_name = self._type.lower().strip()
+        if drug_name.startswith('we'):
+            self._value = self._mult * 35
+        elif drug_name.startswith('me'):
+            self._value = self._mult * 70
+        elif drug_name.startswith('co'):
+            self._value = self._mult * 150
+        else:
+            self._value = 0
+        self._value = round(self._value, 2)
+
+    def get_type(self):
+        return self._type
+
+    def get_mult(self):
+        return self._mult
+
+    def get_value(self):
+        return self._value
+
+    def get_effects(self):
+        l = list(self._effects.keys())
+        l.sort()
+        return l
+
+    def get_added_mixer(self):
+        return self._added_mixer
 
     def small_representation(self):
         first_block = [
@@ -147,79 +223,3 @@ class Drug:
                 lines_to_return.append(f'{'-' * max_amount_chars}')
 
         return lines_to_return
-
-
-    def add_mixer(self, mixer):
-        self._added_mixer = mixer.lower()
-        self._added_effect = self._mixer_dict[self._added_mixer]
-        self.__create_modified_effects_dict(mixer)
-        self.__modify_effects(mixer)
-
-        mult_before = self._mult
-        self.__calc_mult()
-        self._added_mult = round(self._mult - mult_before, 2)
-        self.__calc_value()
-
-
-    def __create_modified_effects_dict(self, mixer):
-        dict = {}
-        for effect in self._effects:
-            if effect in self._modify_dict[mixer]:
-                dict[effect] = self._modify_dict[mixer][effect]
-        self._modified_effects = dict
-
-    def __modify_effects(self, mixer):
-        new_effects = {}
-        for effect in self._effects:
-            if effect in self._modify_dict[mixer]:
-                new_effects[self._modify_dict[mixer][effect]] = self._value_dict[self._modify_dict[mixer][effect]]
-            else:
-                new_effects[effect] = self._effects[effect]
-
-        # We include this because if that effect already exists, we don't want to add it a second time.
-        # We also don't want to have more than the maximum allowed number of effects on a drug.
-        if len(self._effects) < MAX_EFFECTS:
-            new_effects[self._added_effect] = self._value_dict[self._mixer_dict[self._added_mixer]]
-        new_effects = dict(sorted(new_effects.items()))
-        self._effects = new_effects
-
-
-    def __create_dicts(self):
-        self._value_dict = create_values_dict('csv-files/values.csv', 0, 1)
-        self._mixer_dict = create_mixer_dict('csv-files/mixer.csv', 0, 1)
-        self._modify_dict = create_modify_dict('csv-files/modify.csv', 2, 0, 1)
-
-    def __calc_mult(self):
-        total = 1
-        for effect in self._effects:
-            total += self._effects[effect]
-        self._mult = round(total, 2)
-
-    def __calc_value(self):
-        drug_name = self._type.lower().strip()
-        if drug_name.startswith('we'):
-            self._value = self._mult * 35
-        elif drug_name.startswith('me'):
-            self._value = self._mult * 70
-        elif drug_name.startswith('co'):
-            self._value = self._mult * 150
-        else:
-            self._value = 0
-        self._value = round(self._value, 2)
-
-    def get_type(self):
-        return self._type
-
-    def get_mult(self):
-        return self._mult
-
-    def get_value(self):
-        return self._value
-
-    def get_effects(self):
-        l = list(self._effects.keys())
-        l.sort()
-        return l
-
-    def get_added_mixer(self):
-        return self._added_mixer
--- recursion.py
+++ recursion.py
@@ -2,29 +2,38 @@ from dicts import create_mixer_dict
 from drug import Drug
 import time
 
+
 def main():
     mixer_dict = create_mixer_dict('csv-files/mixer.csv', 0, 1)
 
-    drug = Drug('meth', ['toxic'])
+    drug = Drug('meth')
     before = time.time()
-    final_value, mixer_path = find_best_strain_recursively(drug, mixer_dict, 5)
+    final_value, mixer_path = find_best_strain_recursively(drug, mixer_dict, 6)
     after = time.time()
     print(f"\nDone in {after - before:.3f} seconds!")
     print(f"Best value: {final_value}")
     print(f"Best mixer path: {' -> '.join(mixer_path)}")
 
+def clear():
+    """clear the screen and return cursor to home position"""
+    print(end="\x1b[H\x1b[J", flush=True)
 
-def find_best_strain_recursively(drug, mixer_dict, iterations):
+def find_best_strain_recursively(drug, mixer_dict, iterations, mixers=[]):
     if iterations == 0:
         return drug.get_mult(), []  # return the value and an empty path
 
     max_value = 0
     best_path = []
-
+    m = [] + mixers
+    m.append(drug.get_added_mixer())
+    clear()
+    for a in m:
+        print(f'{a:<13}', end='')
+    print('\n', drug.get_mult())
     for mixer in mixer_dict.keys():
         new_drug = Drug(drug.get_type(), drug.get_effects())
         new_drug.add_mixer(mixer)
-        value, path = find_best_strain_recursively(new_drug, mixer_dict, iterations - 1)
+        value, path = find_best_strain_recursively(new_drug, mixer_dict, iterations - 1, m)
 
         if value > max_value:
             max_value = value
